c38c35edd21e5ded99459220974b30076d534ac3
Amen0920 <luj0605@gmail.com>
2025-07-13 17:33:49 +0900

    add: pothosのbuilderを作成する

---
+++ apps/backend/src/schema/builder.ts
@@ -0,0 +1,11 @@
+import PrismaPlugin from "@pothos/plugin-prisma";
+import SchemaBuilder from "@pothos/core";
+import type PrismaTypes from "../../prisma/generated.d.ts";
+import { db } from "@/src/db";
+
+export const builder = new SchemaBuilder<{ PrismaTypes: PrismaTypes }>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: db,
+  },
+});
