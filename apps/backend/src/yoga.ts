22e858854c173540d25d4d10247ce755a5633c73
Amen0920 <luj0605@gmail.com>
2025-07-13 18:34:25 +0900

    update: yoga.tsを修正する

--- apps/backend/src/yoga.ts
+++ apps/backend/src/yoga.ts
@@ -1,11 +1,8 @@
 import { createYoga } from "graphql-yoga";
 import { schema } from "./schema/schema";
-import { db } from "./db";
 
 export const yoga = createYoga({
   graphqlEndpoint: "/",
   schema,
-  context: {
-    client: db,
-  },
+  context: ({ request }) => ({ request }),
 });
