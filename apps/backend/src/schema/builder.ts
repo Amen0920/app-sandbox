9372b8c4916d04c6591f9ebf43fa83348c1ef0d3
Amen0920 <luj0605@gmail.com>
2025-07-14 23:45:06 +0900

    update: userドメインを分離する

--- apps/backend/src/schema/builder.ts
+++ apps/backend/src/schema/builder.ts
@@ -1,7 +1,7 @@
 import PrismaPlugin from "@pothos/plugin-prisma";
 import SchemaBuilder from "@pothos/core";
 import type PrismaTypes from "../../prisma/generated.d.ts";
-import { db } from "@/src/db";
+import { db } from "../db";
 
 export const builder = new SchemaBuilder<{ PrismaTypes: PrismaTypes }>({
   plugins: [PrismaPlugin],
--- apps/backend/src/schema/schema.ts
+++ apps/backend/src/schema/schema.ts
@@ -1,30 +1,8 @@
-import { db } from "@/src/db";
+import "./auth";
 import { builder } from "./builder";
-
-builder.prismaObject("User", {
-  fields: (t) => ({
-    id: t.exposeID("id"),
-    email: t.exposeString("email"),
-  }),
-});
 
 builder.queryType({
   fields: (t) => ({
-    me: t.prismaField({
-      type: "User",
-      nullable: true,
-      args: {
-        email: t.arg.string({ required: true }),
-      },
-      resolve: (query, _, args) => {
-        return db.user.findUnique({
-          ...query,
-          where: {
-            email: args.email,
-          },
-        });
-      },
-    }),
     hello: t.string({
       resolve: () => {
         return "world";
